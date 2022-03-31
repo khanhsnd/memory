ca41385d9351732dcbb3a28ccf85e9541353939f
khanhsnd <duckhanh.0811@gmail.com>
2022-03-31 10:01:45 +0700

    Add files via upload
    init

---
+++ js/light.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    var folder = "img/slider";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        debugger
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("body").append( "<img src='"+ folder + val +"'>" );
+            } 
+        });
+    }
+});
+});
