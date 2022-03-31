9e8e10195ace72fc96bd9a0ee475faa43ba7cd38
Khanh <duckhanh.0811@gmail.com>
2022-03-31 10:41:21 +0700

    ok

--- js/light.js
+++ js/light.js
@@ -1,8 +1,9 @@
 $( document ).ready(function() {
-    var folder = "img/slider";
+    var folder = "./img/slider";
 
 $.ajax({
     url : folder,
+    type: 'Get',
     success: function (data) {
         debugger
         $(data).find("a").attr("href", function (i, val) {
